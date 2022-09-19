551df80f670120fd6dc831289b65b52cf0cf27b4
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-09-19 12:14:11 +0200

    add data from component Profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Profile } from "./components/Profile";
+import { user } from "./Data/profileDate";
 
 export const App = () => {
-return <Profile/>
+  const { username, tag, location, avatar, stats } = user;
+  
+  return <Profile
+    username={username} tag={tag} location={location} avatar = {avatar} stats = {stats}/>
  };
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,28 +1,28 @@
-export const Profile = ({username, tag, location, avatar, stats}) => {
+export const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
   return <div class="profile">
     <div class="description">
       <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        src={avatar}
         alt="User avatar"
         class="avatar"
       />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
+      <p class="name">{username}</p>
+      <p class="tag">{tag}</p>
+      <p class="location">{location}</p>
     </div>
 
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
-        <span class="quantity">1000</span>
+        <span class="quantity">{followers}</span>
       </li>
       <li>
         <span class="label">Views</span>
-        <span class="quantity">2000</span>
+        <span class="quantity">{views}</span>
       </li>
       <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
+        <span class="label">likes</span>
+        <span class="quantity">{likes}</span>
       </li>
     </ul>
   </div>
