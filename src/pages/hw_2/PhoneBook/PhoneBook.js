7710a22b12c873aa80481f2458f75cd6e1814fc9
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-10-10 15:16:49 +0200

    add event

--- src/pages/hw_2/PhoneBook/PhoneBook.js
+++ src/pages/hw_2/PhoneBook/PhoneBook.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 
-export default class PhoneBook  extends Component { 
+export default class PhoneBook extends Component {
   state = {
     contacts: [],
     name: ''
   }
 
-  onInput = (e) => {
-e.preventDefauld();
+  changeInput = (e) => {
+    // console.dir(e);
+    this.setState(prevState => {
+      return {}
+    })
   }
- 
+
   render() {
-   return <div>
-      <input 
-      onInput={this.onInput }
-      type="text"
-      name="name"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      required
-    />
+    return <div>
+      <h1>Phonebook</h1>
+      <input
+        onSubmit={this.changeInpu}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
     </div>
-    
+
   }
 };
