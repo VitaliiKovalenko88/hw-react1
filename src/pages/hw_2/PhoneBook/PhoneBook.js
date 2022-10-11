0a5216ed51d5b054d3b418de2b7516553f6db5e0
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-10-11 12:56:26 +0200

    add new input

--- src/pages/hw_2/PhoneBook/PhoneBook.js
+++ src/pages/hw_2/PhoneBook/PhoneBook.js
@@ -40,6 +40,13 @@ export default class PhoneBook extends Component {
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
         />
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
         <button type="submite">Add contact</button>
       </form> 
       <h2>Contacts</h2>
