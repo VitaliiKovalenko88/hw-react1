7b9ccccd1ddb2f0df76b7819b5f3c1cc3831c385
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-10-27 14:05:25 +0200

    add error and isLoading from render

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { WorkDetails } from "pages/WorkDetails";
 import Feedback from "components/hw-2/Feedback/Feedback/Feedback";
 import PhoneBook from "components/hw-2/PhoneBook/PhoneBook/PhoneBook";
 import FeedbackHooks from "components/hw_3/Feedback/Feedback";
+import Article from "pages/hw_3/Module_Practice/Article";
 
 
 export const App = () => {
@@ -23,6 +24,7 @@ export const App = () => {
         <Route path="feedback" element={<Feedback/>} />
         <Route path="phonebook" element={<PhoneBook/>}/>
         <Route path="feedback_lifecycl" element={<FeedbackHooks/>}/>
+        <Route path="article" element={<Article/>} />
       </Route>
       
       <Route path="*" element={<HomePage />} />
--- src/pages/hw_3/HomeWorkThird.js
+++ src/pages/hw_3/HomeWorkThird.js
@@ -6,6 +6,7 @@ const HomeWorkThird = () => {
     <ul>
       <li key={'feedback'}><Link to={'feedback_lifecycl'}>Feedback</Link></li>
       <li key={'phoneBook'} ><Link to={'phoneBook_lifecycl'} >Phonebook</Link></li>
+      <li key={'practice'}><Link to={'article'}>Article</Link> </li>
     </ul>
     <Outlet /></>
 }
