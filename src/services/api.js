eaf1b1b29ebf57ab2defd9bce4c7942d020d64f8
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-11-02 23:04:45 +0100

    create API for gallary

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const API_KEY = '22469389-b601f8ee0455705c879309776';
+const BASE_URL = 'https://pixabay.com/api';
+
+
  export const fetchArticlesWithQuery = async searchQuery => {
   const response = axios.get(`/search?query=${searchQuery}`);
   const data = await response;
@@ -7,6 +11,10 @@ import axios from "axios";
   
   return hits;
 };
+
+export const getImageFromQuery = async (query, page) => {
+  const response = axios.get(`${BASE_URL}/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
