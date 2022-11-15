1951747e87c259f1634579b87550306816585cdb
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-11-16 00:08:36 +0100

    create styaled for ImageGallerry

--- src/components/hw_3/GallerySearch/ImageGallery/ImageGallery.js
+++ src/components/hw_3/GallerySearch/ImageGallery/ImageGallery.js
@@ -1,11 +1,12 @@
+import { StyledImageGallery } from "./ImageGallery.styled";
 import { ImageGalleryItem } from "./ImageGalleryItem/ImageGalleryItem";
 
 export const ImageGallery = ({ gallery, onClick }) => {
   return (
-    <ul onClick={onClick} >
+    <StyledImageGallery onClick={onClick} >
       {gallery.map(({ id, webformatURL, tags, largeImageURL }) => {
         return <ImageGalleryItem key={id} url={webformatURL} tags={tags} modalUrl={largeImageURL} />
       })}
-    </ul>
+    </StyledImageGallery>
   )
  };
