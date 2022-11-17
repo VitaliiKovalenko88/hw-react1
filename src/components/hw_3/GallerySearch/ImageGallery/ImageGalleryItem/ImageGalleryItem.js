29407104a42978a98b40782d02d0c67809220345
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-11-17 22:26:21 +0100

    add new styles for ImageGalleryItem

--- src/components/hw_3/GallerySearch/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/hw_3/GallerySearch/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,6 +1,8 @@
+import { Image, ImageItem } from "./ImageGalleryItem.styled";
+
 export const ImageGalleryItem = ({ url, tags, modalUrl}) => {
   return (
-    <li>
-      <img data-image={modalUrl} src={url} alt={tags} width='200' />
-    </li>)
+    <ImageItem>
+      <Image data-image={modalUrl} src={url} alt={tags} width='200' />
+    </ImageItem>)
 };
